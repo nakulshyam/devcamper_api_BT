5d68644a24774b7e654b84eb4c198159c1e03c8c
Ripetaurus <lactic.nakul@gmail.com>
2020-03-23 22:36:29 +0530

    add slugify

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema({
 	name: {
@@ -98,6 +99,13 @@ const BootcampSchema = new mongoose.Schema({
 		type: Date,
 		default: Date.now
 	}
+});
+
+// Create bootcamp slug from name
+BootcampSchema.pre('save', function(next) {
+	this.slug = slugify(this.name, { lower: true });
+	console.log(this.slug);
+	next();
 });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
