95873f6fe290cd720a937673149613917a648392
Ripetaurus <lactic.nakul@gmail.com>
2020-03-26 22:51:26 +0530

    add review model and getreviews controller

---
+++ routes/reviews.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const { getReviews } = require('../controllers/reviews');
+
+const Review = require('../models/Review');
+
+const router = express.Router({ mergeParams: true });
+
+const advancedResults = require('../middleware/advancedresults');
+const { protect, authorize } = require('../middleware/auth');
+
+router.route('/').get(
+	advancedResults(Review, {
+		path: 'bootcamp',
+		select: 'name description'
+	}),
+	getReviews
+);
+// 	.post(protect, authorize('publisher', 'admin'), addCourse);
+// router
+// 	.route('/:id')
+// 	.get(getCourse)
+// 	.put(protect, authorize('publisher', 'admin'), updateCourse)
+// 	.delete(protect, authorize('publisher', 'admin'), deleteCourse);
+
+module.exports = router;
